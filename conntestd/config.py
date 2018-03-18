028d1041da1b51646e1c3553b9d25ee0914b6ea7
Robert Putt <robert.putt@rackspace.co.uk>
2018-03-18 16:21:35 +0000

    Shove the sqlite file as a dot file in the user's home.

--- conntestd/config.py
+++ conntestd/config.py
@@ -1,2 +1,6 @@
-SECRET_KEY = 'bla'
-DB_CONN = 'sqlite:///conntestd.db'
+import uuid
+from os.path import expanduser
+
+SECRET_KEY = str(uuid.uuid4())
+HOME = expanduser('~')
+DB_CONN = 'sqlite:////%s/.conntestd.db' % HOME
